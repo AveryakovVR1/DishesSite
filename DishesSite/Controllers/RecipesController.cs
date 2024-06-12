12c1f13a87b3eef6c5e356547e189290f105d86a
kolob <kolob@VLAD>
2024-06-12 22:36:38 +0700

    Добавьте файлы проекта.

---
+++ DishesSite/Controllers/RecipesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DishesSite.DataContext;
+using DishesSite.Models;
+
+namespace DishesSite.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly DishesSiteContext _context;
+
+        public RecipesController(DishesSiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recipes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Рецепты.ToListAsync());
+        }
+
+        // GET: Recipes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Рецепты
+                .FirstOrDefaultAsync(m => m.Рецепт_Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+        // GET: Recipes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Рецепты
+                .FirstOrDefaultAsync(m => m.Рецепт_Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // POST: Recipes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recipe = await _context.Рецепты.FindAsync(id);
+            if (recipe != null)
+            {
+                _context.Рецепты.Remove(recipe);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeExists(int id)
+        {
+            return _context.Рецепты.Any(e => e.Рецепт_Id == id);
+        }
+    }
+}
---
+++ DishesSite/Models/Rating.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DishesSite.Models
+{
+    public class Rating
+    {
+        [Key]
+        public int Рейтинг_Id { get; set; }
+
+        public decimal Средний_рейтинг { get; set; }
+
+        public int Количество_оценок { get; set; }
+
+        // Внешний ключ на таблицу Рецепты
+        [ForeignKey("Recipe")]
+        public int Рецепт_Id { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
